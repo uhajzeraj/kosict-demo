f464f03d2d2291ab901f55fbe9c2579acbed46b3
Uran Hajzeraj <uran@frakton.com>
2020-12-08 23:12:14 +0100

    tests

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+require __DIR__.'/auth.php';
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
